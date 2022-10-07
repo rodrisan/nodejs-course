3ac0a547ccc4ea912da190a4c0382b39e5baa8e1
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2022-10-07 08:42:36 -0500

    add express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My App');
+const express = require('express');
+const app = express();
+const port = 3020;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(port, () => {
+  console.log('Port: ' + port);
+});
