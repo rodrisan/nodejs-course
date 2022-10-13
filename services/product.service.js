196739fee30569bdd8f69a15c3afb2196826101a
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2022-10-13 08:57:12 -0500

    add sequelize

--- services/product.service.js
+++ services/product.service.js
@@ -1,15 +1,13 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductService {
 
   constructor(){
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.log(err));
   }
 
   async generate() {
@@ -37,8 +35,8 @@ class ProductService {
 
   async find() {
     const query = 'SELECT * from tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
