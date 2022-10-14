0b0fdff8483272500ab3e498909e688b807768b5
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2022-10-14 08:45:21 -0500

    add alter table migration

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,15 +3,18 @@ const Joi = require('joi');
 const id = Joi.number();
 const email= Joi.string().email();
 const password = Joi.string().min(8).max(100);
+const role = Joi.string().min(5);
 
 const createUserSchema = Joi.object({
   email: email.required(),
   password: password.required(),
+  role: role.required(),
 });
 
 const updateUserSchema = Joi.object({
   email: email,
   password: password,
+  role: role,
 });
 
 const getUserSchema = Joi.object({
